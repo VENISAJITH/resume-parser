e1424c6a665a8e385fdeaee1ba4a01615702f4a9
VENISAJITH <60598276+VENISAJITH@users.noreply.github.com>
2021-07-10 13:02:05 +0530

    Update resume.py

--- resume.py
+++ resume.py
@@ -1,18 +1,30 @@
-import warnings
-warnings.filterwarnings('ignore')
-import en_core_web_sm
-nlp=en_core_web_sm.load()
-import PyPDF2
-import os
-import nltk
-from pyresparser import ResumeParser
-dic=[]
-data=ResumeParser('resume.pdf').get_extracted_data()
-data
-dic.append(data)
-dic
+from logging import error
 import pandas as pd
-d={}
-d=dic
-df=pd.DataFrame(d)
-df
+import mysql.connector
+from mysql.connector import Error
+from mysql.connector import connection
+from mysql.connector import cursor
+import warnings
+import en_core_web_sm
+import PyPDF2
+import nltk
+import os
+import sqlalchemy
+import pymysql
+from sqlalchemy import create_engine
+from sqlalchemy import engine
+from sqlalchemy.sql.functions import user
+nlp=en_core_web_sm.load()
+warnings.filterwarnings('ignore')
+from pyresparser import ResumeParser
+try:
+    data = ResumeParser('./resume.pdf').get_extracted_data()
+    dic=[]
+    dic.append(data)
+    d={}
+    d=dic
+    df=pd.DataFrame(d)
+    print(df)
+except Error as f:
+    print(f)
+
